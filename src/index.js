6c7ba9a5f97575fb267fa5cf370d3aa67c8173a9
Simon Mundy <simon.mundy@peptolab.com>
2025-09-12 00:31:11 +1000

    WIP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-// Parallelogram-JS - Enhancement-First JavaScript Framework
 export { ComponentRegistry } from './core/ComponentRegistry.js';
-export { EventManager } from './core/EventManager.js';
-export { DevLogger } from './core/DevLogger.js';
-export { BaseComponent } from './core/BaseComponent.js';
+export { EventManager } from './managers/EventManager.js';
 export { RouterManager } from './managers/RouterManager.js';
 export { PageManager } from './managers/PageManager.js';
-export { AlertManager } from './managers/AlertManager.js';
-
-export { PSelect } from './components/PSelect.js';
-export { PToasts } from './components/PToasts.js';
-export { PModal } from './components/PModal.js';
-
-export { Carousel } from './components/Carousel.js';
-export { Lazyimage } from './components/Lazyimage.js';
-export { Modal } from './components/Modal.js';
-export { Scrollreveal } from './components/Scrollreveal.js';
-export { Scrollhide } from './components/Scrollhide.js';
-export { Tabs } from './components/Tabs.js';
-export { Toast } from './components/Toast.js';
-export { Toggle } from './components/Toggle.js';
-export { Videoplay } from './components/Videoplay.js';
