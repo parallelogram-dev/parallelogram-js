3b1df23911b7223838bf63792ec5192c14f5bc8a
Simon Mundy <simon.mundy@peptolab.com>
2025-09-12 00:46:26 +1000

    WIP

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { ComponentRegistry } from './core/ComponentRegistry.js';
 export { EventManager } from './managers/EventManager.js';
 export { RouterManager } from './managers/RouterManager.js';
 export { PageManager } from './managers/PageManager.js';
+export { BaseComponent } from './core/BaseComponent.js';
