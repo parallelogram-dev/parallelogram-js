f4f78b3e1f6efe5add6af511312a30a80a372a50
Simon Mundy <simon.mundy@peptolab.com>
2026-09-15 22:24:56 +1000

    Set the documentation site in Haffer instead of Matter SQ
    Haffer Regular for text and Haffer Medium for headings and emphasis, self-hosted
    under PeptoLab's Displaay web licence. The Matter SQ files are removed.

--- site/build/render.js
+++ site/build/render.js
@@ -92,7 +92,7 @@ export function layout({ title, description, current, contracts, guides, content
 <link rel="icon" href="favicon.svg" type="image/svg+xml">
 <link rel="icon" href="favicon.png" type="image/png" sizes="48x48">
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
-<link rel="preload" href="fonts/MatterSQ-Regular.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="fonts/Haffer-Regular.woff2" as="font" type="font/woff2" crossorigin>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&display=swap">
